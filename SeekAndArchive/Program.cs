aead1b98aa4f725f0f527ace1335fd060fbb5f9a
tarakarcsi <tarakarcsi94@gmail.com>
2019-08-19 12:07:37 +0200

    recursive search method

--- SeekAndArchive/Program.cs
+++ SeekAndArchive/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,37 @@ namespace SeekAndArchive
 {
     class Program
     {
+
+        private static List<FileInfo> FoundFiles;
+        private static List<FileSystemWatcher> watchers;
+        private static List<DirectoryInfo> archiveDirs;
+
         static void Main(string[] args)
         {
         }
+
+
+        public static void RecursiveSearch(List<FileInfo> foundFiles, string fileName, DirectoryInfo currentDirectory)
+        {
+            foreach (FileInfo file in currentDirectory.GetFiles())
+            {
+                if (file.Name == fileName)
+                {
+                    foundFiles.Add(file);
+                }
+            }
+
+            foreach (DirectoryInfo dir in currentDirectory.GetDirectories())
+            {
+                RecursiveSearch(foundFiles, fileName, dir);
+            }
+        }
+
+        public static void WatcherChanged(object sender, FileSystemEventArgs e)
+        {
+
+        }
     }
+
+    
 }
